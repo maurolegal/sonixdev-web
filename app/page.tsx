1192c7ac4892f9a9322d669976289934783f3cf3
nemesisxpert <maurochica23@gmail.com>
2026-07-06 09:54:48 -0500

    feat(app): add home page entry

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/components/hero/Hero';
+import Navbar from '@/components/layout/Navbar';
+
+export default function HomePage() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
